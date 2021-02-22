d8e2795d932ddf314727ae0f59b1c0e09c1cb66b
Javier Estevez <javierdaniloestevez@gmail.com>
2021-02-22 04:29:37 -0300

    agregado pipe currency UYU y pipe json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,15 @@ export class AppComponent {
   nombre    : string = 'Capitán América';
   PI        : number = Math.PI;
   porcentaje: number = 0.234;
+  salario   : number = 1234.5;
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: 20
+    }
+  }
 }
