295e962b3a5ec8a9afe6deb895c68b25290c2cda
Javier Estevez <javierdaniloestevez@gmail.com>
2021-02-24 05:07:06 -0300

    uso de fechas en español, pesos uruguayos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+// Agregar estos imports y la funcion registerLocaleData para usar las fechas en español y la moneda de uruguay
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es-UY';
+
+registerLocaleData(localeEs);
+// hasta aqui todo lo que se agrega para usar la moneda de uruguay, junto con el import de LOCALE_ID y el providers
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +17,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ { provide: LOCALE_ID, useValue: 'es-UY' } ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
